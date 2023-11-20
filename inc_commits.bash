2b72b2fbf22dfe5d00097bc112cd1713b13a96c1
Abdelrhman Mohamed <33766090+MegaMind2999@users.noreply.github.com>
2023-11-20 05:25:15 +0200

    Update inc_commits.bash

--- inc_commits.bash
+++ inc_commits.bash
@@ -1,5 +1,2 @@
 #!/bin/bash
-for i in {1..5}
-do
-   echo $(($RANDOM%(100000-1)+1)) > README.md && git add -A && git commit -m "new" && git push
-done
+for i in {1..5}; do    echo $(($RANDOM%(100000-1)+1)) > README.md && git add -A && git commit -m "new" && git push; done
